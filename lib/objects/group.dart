04ecb34dbd3f5ac866597be31cecd41c704eedae
Ulrik Haaland <ulrikhaaland2@gmail.com>
2019-06-05 10:39:20 +0200

    d

--- lib/objects/group.dart
+++ lib/objects/group.dart
@@ -1,20 +1,20 @@
-import 'user.dart';
+// import 'user.dart';
 
-class Group {
-  Group({
-    this.memberList,
-    this.challengesList,
-    this.chatList,
-    this.eventsList,
-    this.id,
-    this.name,
-  });
+// class Group {
+//   Group({
+//     this.memberList,
+//     this.challengesList,
+//     this.chatList,
+//     this.eventsList,
+//     this.id,
+//     this.name,
+//   });
 
-  final String id;
-  final String name;
+//   final String id;
+//   final String name;
 
-  final List<User> memberList;
-  final List<Event> eventsList;
-  final List<Challenge> challengesList;
-  final List<Chat> chatList;
-}
+//   final List<User> memberList;
+//   final List<Event> eventsList;
+//   final List<Challenge> challengesList;
+//   final List<Chat> chatList;
+// }
